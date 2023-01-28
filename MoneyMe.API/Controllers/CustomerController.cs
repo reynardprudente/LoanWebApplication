c5069c346bb72e6b899b7a929d0431cf9359900e
Reynard Prudente <reynard.prudente@betterask.erni>
2023-01-28 07:59:04 +0100

    Add project files.

---
+++ MoneyMe.API/Controllers/CustomerController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace MoneyMe.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> AddCustomer()
+        {
+            return Ok();
+        }
+    }
+}
